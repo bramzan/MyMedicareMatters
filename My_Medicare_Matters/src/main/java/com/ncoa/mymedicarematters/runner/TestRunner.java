501725ba20b88ab15818e2d4afad135b11700ad9
evan.zimmerman <evan.zimmerman@NCAWKLT231768.ncoa.local>
2020-12-18 15:07:36 -0500

    updat

--- My_Medicare_Matters/src/main/java/com/ncoa/mymedicarematters/runner/TestRunner.java
+++ My_Medicare_Matters/src/main/java/com/ncoa/mymedicarematters/runner/TestRunner.java
@@ -8,20 +8,17 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions( 
 
-<<<<<<< HEAD
+
 		features = "C:\\Users\\evan.zimmerman\\git\\MyMedicareMatters\\My_Medicare_Matters\\src\\main\\resources\\TestFeatures",
-=======
-		features = "C:\\Users\\Muhammad.Ramzan\\GitRepository\\MyMedicareMatters\\My_Medicare_Matters\\src\\main\\resources\\TestFeatures",
->>>>>>> branch 'main' of https://github.com/bramzan/MyMedicareMatters.git
+		//features = "C:\\Users\\Muhammad.Ramzan\\GitRepository\\MyMedicareMatters\\My_Medicare_Matters\\src\\main\\resources\\TestFeatures",
+
 		glue = {"com.ncoa.mymedicarematters.stepdefinition"},
 		//format = {"pretty","html:test-outout"},
 		plugin = {"pretty","html:target/cucumber-html-report", "json:target/cucumber.json"},//output and reporting
 		monochrome = true,
-<<<<<<< HEAD
-		tags = {"@Homepage_Footer_Connect_With_Us"}
-=======
+
 		tags = {"@UnderstandEnrollment"}
->>>>>>> branch 'main' of https://github.com/bramzan/MyMedicareMatters.git
+
 		//dryRun = false  // 
 		
 
